f37cbe7fb946403fccf745005639ebc3eaccaf47
whiterthanwhite <0whiterthanwhite0@gmail.com>
2022-04-20 02:56:00 +0300

    inc04

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,10 +1,8 @@
 package handlers
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 
 	//"io/ioutil"
 	"log"
@@ -181,23 +179,23 @@ func GetAllMetricsFromServer(serverMetrics []metrics.Metrics) http.HandlerFunc {
 
 func UpdateMetricOnServer(serverMetrics map[string]metrics.Metrics) http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
-		requestBody, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, fmt.Sprint(err), http.StatusInternalServerError)
-			return
-		}
-		r.Body.Close()
-		log.Println(string(requestBody))
-
-		var requestMetric metrics.Metrics
 		/*
+			requestBody, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				http.Error(rw, fmt.Sprint(err), http.StatusInternalServerError)
+				return
+			}
+			r.Body.Close()
+
+			var requestMetric metrics.Metrics
 			if err := json.Unmarshal(requestBody, &requestMetric); err != nil {
 				http.Error(rw, fmt.Sprint(err), http.StatusInternalServerError)
 				return
 			}
 		*/
-		if err := json.NewDecoder(bytes.NewBuffer(requestBody)).Decode(&requestMetric); err != nil {
-			log.Println(string(requestBody))
+
+		var requestMetric metrics.Metrics
+		if err := json.NewDecoder(r.Body).Decode(&requestMetric); err != nil {
 			http.Error(rw, fmt.Sprint(err), http.StatusInternalServerError)
 			return
 		}
