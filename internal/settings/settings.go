5dc203adda554769516c8a160666531c397dde91
whiterthanwhite <0whiterthanwhite0@gmail.com>
2022-04-21 23:09:05 +0300

    inc05

--- internal/settings/settings.go
+++ internal/settings/settings.go
@@ -2,56 +2,20 @@ package settings
 
 import (
 	"log"
-	"os"
-	"strconv"
-)
 
-const (
-	defaultPollInterval   = "2"
-	defaultReportInterval = "10"
-	defaultAddress        = "localhost:8080"
+	"github.com/caarlos0/env"
 )
 
 type SysSettings struct {
-	Address        string
-	PollInterval   int64
-	ReportInterval int64
+	Address        string `env:"ADDRESS" envDefault:"localhost:8080"`
+	PollInterval   int64  `env:"POLL_INTERVAL" envDefault:"2"`
+	ReportInterval int64  `env:"REPORT_INTERVAL" envDefault:"10"`
 }
 
 func GetSysSettings() SysSettings {
 	sysSettings := SysSettings{}
-	sysSettings.Address = getEnvVariable("ADDRESS")
-	var err error
-	v := getEnvVariable("REPORT_INTERVAL")
-	sysSettings.ReportInterval, err = strconv.ParseInt(v, 0, 8)
-	if err != nil {
-		log.Fatal(err)
-	}
-	v = getEnvVariable("POLL_INTERVAL")
-	sysSettings.PollInterval, err = strconv.ParseInt(v, 0, 8)
-	if err != nil {
+	if err := env.Parse(&sysSettings); err != nil {
 		log.Fatal(err)
 	}
 	return sysSettings
-}
-
-func getEnvVariable(envName string) string {
-	v := os.Getenv(envName)
-	if v == "" {
-		defaultValue := ""
-		switch envName {
-		case "ADDRESS":
-			defaultValue = defaultAddress
-		case "REPORT_INTERVAL":
-			defaultValue = defaultReportInterval
-		case "POLL_INTERVAL":
-			defaultValue = defaultPollInterval
-		}
-
-		if err := os.Setenv(envName, defaultValue); err != nil {
-			log.Fatal(err)
-		}
-		v = defaultValue
-	}
-	return v
 }
