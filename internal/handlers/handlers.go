3400feb8ceb58d734534ec8d085540e54120b8e3
whiterthanwhite <0whiterthanwhite0@gmail.com>
2022-05-09 21:01:38 +0300

    inc14_03

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -354,15 +354,17 @@ func UpdateMetricsOnServer(serverMetrics map[string]metrics.Metrics, serverSetti
 				connCtx, connCancel := context.WithTimeout(mdb.GetDBContext(), 5*time.Second)
 				switch m.MType {
 				case metrics.CounterType:
+					log.Printf("Try: metric %v of type %v with value %v", m.ID, m.MType, m.Delta)
 					if _, err := tx.Exec(connCtx, "insert into metrics values ($1, $2, $3, $4)", m.ID, m.MType, *m.Delta, nil); err != nil {
 						log.Println(err.Error())
 					}
-					log.Printf("insert metric %v of type %v with value %v", m.ID, m.MType, *m.Delta)
+					log.Printf("Success: metric %v of type %v with value %v", m.ID, m.MType, *m.Delta)
 				case metrics.GaugeType:
+					log.Printf("Try: metric %v of type %v with value %v", m.ID, m.MType, m.Value)
 					if _, err := tx.Exec(connCtx, "insert into metrics values ($1, $2, $3, $4)", m.ID, m.MType, nil, *m.Value); err != nil {
 						log.Println(err.Error())
 					}
-					log.Printf("insert metric %v of type %v with value %v", m.ID, m.MType, *m.Value)
+					log.Printf("Success: metric %v of type %v with value %v", m.ID, m.MType, *m.Value)
 				}
 				connCancel()
 			}
